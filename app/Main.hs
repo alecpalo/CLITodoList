f40676b860584409669f115d0a9e4b55acba0be4
Alec Palo <alecpalo@Alecs-MacBook-Pro-4.local>
2024-03-20 22:37:30 -0400

    some progress

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,14 @@
-module Main where
+import Control.Monad (unless)
+
+prompt :: IO ()
+prompt = do
+  i <- getLine
+  unless(i == "q") $ do
+    putStrLn("you entered " ++ i ++ ".")
+    prompt
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  putStrLn "Welcome to your task list! \nWhat would you like to do today?"
+  putStrLn "press q to quit!"
+  prompt
